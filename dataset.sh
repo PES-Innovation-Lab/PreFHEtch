320d85e75b7f55057f618f4de3b2807946ed5ac7
Tejas R <tejas.techstack@gmail.com>
2025-07-10 00:35:04 +0530

    chore: update readme, add shell script for dataset, add dependency to Dockerfile

---
+++ dataset.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# if dataset does not exist pull it
+if [ ! -d "./sift/siftsmall/" ]; then
+  wget ftp://ftp.irisa.fr/local/texmex/corpus/siftsmall.tar.gz
+  tar -xvf siftsmall.tar.gz
+  rm siftsmall.tar.gz
+  mkdir sift 
+  mv siftsmall sift/
+fi
